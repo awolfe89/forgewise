41765b0cf8e199b187a027c2d818b9dc03e82c6a
awolfe89 <allan.wolfe89@gmail.com>
2025-10-06 08:59:42 -0500

    Fix missing Image import causing build failure
    Added missing next/image import to resolve ReferenceError during static page generation.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useState, lazy, Suspense } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { StaggerContainer, StaggerItem } from '../src/components/AnimatedComponents';
 import { BookingLink } from '../src/components/ProtectedContact';
